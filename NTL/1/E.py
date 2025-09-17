43af8f2ac6b4d7505777a89c7b3d33af9189fbe1
ibukye <ramnjn0112mk@gmail.com>
2025-09-18 07:51:45 +0800

    ?

---
+++ NTL/1/E.py
@@ -0,0 +1,14 @@
+a,b=map(int,input().split())
+def solve_euclid(a,b):
+    x=0,y=0
+    min=10000000000000
+    for i in range()
+
+
+    if sum_abs(x,y)<min:
+        ans_x,ans_y=x,y
+        min=sum_abs
+    
+
+def sum_abs(x,y):
+    return abs(x)+abs(y)
--- NTL/1/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-n=int(input())
-cnt=0
-for i in range(1,n):
-    if n%i!=0:cnt+=1
-print(cnt)
